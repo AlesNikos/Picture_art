b1983e53773a30d6c771b04fbcc19054896187b0
Alexey Kostyuchik <alesnikos@gmail.com>
2018-09-30 22:00:24 +0500

    commit

--- js/bundle/bundle.js
+++ js/bundle/bundle.js
@@ -354,26 +354,25 @@ function sortPortfolio() {
 
 	portfolioMenu.addEventListener('click', function(event) {
 		let target = event.target.className;
-		console.log(target);
-
+		
 		for (let i = 0; i < linkItem.length; i++) {
 
 			if (linkItem[i].classList.contains(target)) {
 				linkItem[i].classList.add('active');
 
-			  	for (let j=0; j<portfolioBlock.length; j++) {
+		  	for (let j = 0; j  <portfolioBlock.length; j++) {
 
-				  	if (portfolioBlock[j].classList.contains(target)) {
-				    	portfolioBlock[j].style.display = 'block';
+			  	if (portfolioBlock[j].classList.contains(target)) {
+			    	portfolioBlock[j].style.display = 'block';
 
-				    } else if (target == 'grandmother' || target == 'granddad') {
-						portfolioNo.style.display = 'block';
-						portfolioBlock[j].style.display = 'none';
+			    } else if (target == 'grandmother' || target == 'granddad') {
+										portfolioNo.style.display = 'block';
+										portfolioBlock[j].style.display = 'none';
 
-					}	else {
-				    	portfolioBlock[j].style.display = 'none';
-				    	portfolioNo.style.display = 'none';
-				    }
+				}	else {
+			    	portfolioBlock[j].style.display = 'none';
+			    	portfolioNo.style.display = 'none';
+			    }
 				}
 
 			} else {
