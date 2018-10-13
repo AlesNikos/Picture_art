08898487345d1104bab41296f2d60f396faa7325
Alexey Kostyuchik <alesnikos@gmail.com>
2018-10-13 15:39:56 +0500

    commit

--- js/parts/modalGift.js
+++ js/parts/modalGift.js
@@ -4,13 +4,22 @@ function modalGift() {
 
 	let fixedGift = document.querySelector('.fixed-gift'),
 		popupGift = document.querySelector('.popup-gift'),
-		close = document.getElementsByClassName('popup-close');
+		close = document.getElementsByClassName('popup-close'),
+		buttonAll = document.getElementsByTagName('button'),
+		isClicker = false;
 
 	fixedGift.addEventListener('click', openModalGift);
 
 	close[1].addEventListener('click', closeModal);
 
 	popupGift.addEventListener('click', outsideClick);
+
+	document.body.addEventListener('click', event => {
+		let target = event.target;
+		if (!event.target.classList.contains('button')) return;
+		
+		isClick();
+	});
 
 	//Функция открывания модального и исчезновение подарка
 	function openModalGift() {
@@ -29,6 +38,24 @@ function modalGift() {
 			popupGift.style.display = "none";
 		}
 	}
+	//Функция запускается при нажатии на любую кнопку
+	function isClick() {
+		isClicker = true;
+	}
+
+	window.onscroll = function() {
+		// размер скролла
+  let scrolled = window.pageYOffset || document.documentElement.scrollTop,
+  	// высота окна
+	  	heightWindow = window.innerHeight || document.documentElement.clientHeight,
+  	// высота всего документа
+ 	  heightDocument = document.body.clientHeight;
+
+		if ( scrolled + heightWindow >= heightDocument && isClicker == false) {
+			popupGift.style.display = "block";
+			fixedGift.style.display = "none";
+		}
+	}	
 }
 
 module.exports = modalGift;
